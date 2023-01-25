6c12fba48b9a1fed07f179ed99a9df26f660c055
Yakov-Varnaev <yakov.varnaev@mail.ru>
2023-01-26 02:00:28 +0300

    basic setup

---
+++ bbq_manager/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='docs')),
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/jwt', include('djoser.urls.jwt')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+]
