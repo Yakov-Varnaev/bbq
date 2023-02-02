749b268ea33c5b3c60c98e91af090942bb4ef691
versuffer <versuffer@mail.ru>
2023-02-02 19:55:11 +0300

    Creating Company viewset

--- company/views.py
+++ company/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CompanySerializer
+from .models import CompanyModel
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = CompanyModel.objects.all()
