ee717331752acbc12eaafd5a3dbf163944225b75
versuffer <versuffer@mail.ru>
2023-02-02 19:24:31 +0300

    Creating Company model

--- company/models.py
+++ company/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
