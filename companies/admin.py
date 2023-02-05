f66e43bbbbff8647d0a170fa9051de6ce89698e4
versuffer <versuffer@mail.ru>
2023-02-05 16:09:34 +0300

    Adding @admin decorator

--- companies/admin.py
+++ companies/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import CompaniesModel
 
 
+@admin.register(CompaniesModel)
 class CompaniesAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'owner', 'time_created', 'time_updated']
 
-
-admin.site.register(CompaniesModel, CompaniesAdmin)
