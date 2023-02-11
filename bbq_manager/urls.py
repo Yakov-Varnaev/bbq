3e772e0a88e064d0bb093ab1ba09f6db1ac34282
versuffer <versuffer@mail.ru>
2023-02-12 01:08:58 +0300

    Clean up typo in urls.py

--- bbq_manager/urls.py
+++ bbq_manager/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api/', RedirectView.as_view(pattern_name='docs')),
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
-    path('api/auth/jwt', include('djoser.urls.jwt')),
+    path('api/auth/', include('djoser.urls.jwt')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/companies/', include('companies.urls')),
