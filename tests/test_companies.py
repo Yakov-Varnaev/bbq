855bc08746b82657f2793b6549dfd433a65067eb
versuffer <versuffer@mail.ru>
2023-02-12 00:00:05 +0300

    Added test_delete test

--- tests/test_companies.py
+++ tests/test_companies.py
@@ -32,3 +32,14 @@ class TestCRUD:
 
         assert first_client.put('/api/companies/1/', data=put_data).status_code == status.HTTP_200_OK
         assert second_client.put('/api/companies/1/', data=put_data).status_code != status.HTTP_200_OK
+
+    def test_delete(self, auth_setup):
+
+        first_client, second_client = auth_setup
+
+        post_data = {'name': 'TestCompany', 'owner': 1}
+
+        first_client.post('/api/companies/', data=post_data)
+
+        assert second_client.delete('/api/companies/1/').status_code != status.HTTP_204_NO_CONTENT
+        assert first_client.delete('/api/companies/1/').status_code == status.HTTP_204_NO_CONTENT
