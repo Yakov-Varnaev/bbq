8b3d46f8801087e2f46f8db99bf2b3f8d113075d
versuffer <versuffer@mail.ru>
2023-02-12 12:03:10 +0300

    auth_setup refactoring

--- fixtures/companies.py
+++ fixtures/companies.py
@@ -9,13 +9,20 @@ User = get_user_model()
 
 @pytest.fixture
 def auth_setup(auth_client):
+    """
+    This fixture creates an additional user with it's APIClient to test expected CRUD permissions.
+
+    Returns
+    -------
+
+    authorized_client - APIClient of user with expected CREATE, READ, UPDATE, DELETE permissions
+    auth_client - APIClient of user with expected CREATE, READ permissions
+    """
 
     authorized_user = mixer.blend(User)
     authorized_user_token = RefreshToken.for_user(authorized_user).access_token
 
     authorized_client = APIClient()
-    unauthorized_client = auth_client
-
     authorized_client.credentials(HTTP_AUTHORIZATION=f'JWT {authorized_user_token}')
 
-    return authorized_client, unauthorized_client
+    return authorized_client, auth_client
