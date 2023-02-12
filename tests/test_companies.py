b48ed7f9be93de760918fdbbd81f22eb228fe7ad
versuffer <versuffer@mail.ru>
2023-02-12 22:07:11 +0300

    Added data check for GET-request test - test_get_list

--- tests/test_companies.py
+++ tests/test_companies.py
@@ -1,13 +1,27 @@
 import pytest
+import json
 from rest_framework import status
 from django.urls import reverse
+from companies.models import Company
+from companies.serializers import CompanySerializer
 
 
 @pytest.mark.django_db
 class TestCRUD:
 
-    def test_get(self, auth_client):
-        assert auth_client.get(reverse('companies-list')).status_code == status.HTTP_200_OK
+    def test_get_list(self, multiple_companies, auth_client):
+
+        for company in multiple_companies:
+            company.save()
+
+        response = auth_client.get(reverse('companies-list'))
+
+        assert response.status_code == status.HTTP_200_OK
+
+        db_data = CompanySerializer(Company.objects.all(), many=True).data
+        response_data = json.loads(response.content)
+
+        assert db_data == response_data
 
     def test_post(self, auth_client):
         post_data = {'name': 'TestCompany'}
