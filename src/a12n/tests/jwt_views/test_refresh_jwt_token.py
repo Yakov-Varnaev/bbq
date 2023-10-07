6004be600d015213d11e4b3ef9fec516b14cfcbd
Yakov Varnaev <yakov.varnaev@gmail.com>
2023-10-07 11:13:01 +0300

    refresh token tests fix

--- src/a12n/tests/jwt_views/test_refresh_jwt_token.py
+++ src/a12n/tests/jwt_views/test_refresh_jwt_token.py
@@ -1,6 +1,7 @@
 import pytest
 
 from freezegun import freeze_time
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from a12n.utils import get_jwt
 
@@ -12,11 +13,11 @@ pytestmark = [
 
 @pytest.fixture
 def refresh_token(as_user):
-    def _refresh_token(token, expected_status=201):
+    def _refresh_token(token, expected_status=200):
         return as_user.post(
-            "/api/v1/auth/token/refresh/",
+            "/api/v1/auth/jwt/refresh/",
             {
-                "token": token,
+                "refresh": token,
             },
             format="json",
             expected_status=expected_status,
@@ -27,44 +28,43 @@ def refresh_token(as_user):
 
 @pytest.fixture
 def initial_token(as_user):
-    with freeze_time("2049-01-03"):
-        return get_jwt(as_user.user)
+    return str(RefreshToken.for_user(as_user.user))
 
 
 def test_refresh_token_ok(initial_token, refresh_token):
     result = refresh_token(initial_token)
 
-    assert "token" in result
+    assert "access" in result
 
 
 def test_refreshed_token_is_a_token(initial_token, refresh_token):
     result = refresh_token(initial_token)
 
-    assert len(result["token"]) > 32
+    assert len(result["access"]) > 32
 
 
 def test_refreshed_token_is_new_one(initial_token, refresh_token):
     result = refresh_token(initial_token)
 
-    assert result["token"] != initial_token
+    assert result["access"] != initial_token
 
 
 def test_refresh_token_fails_with_incorrect_previous_token(refresh_token):
-    result = refresh_token("some-invalid-previous-token", expected_status=400)
+    result = refresh_token("some-invalid-previous-token", expected_status=401)
 
-    assert "nonFieldErrors" in result
+    assert "detail" in result
 
 
 def test_token_is_not_allowed_to_refresh_if_expired(initial_token, refresh_token):
     with freeze_time("2049-02-05"):
-        result = refresh_token(initial_token, expected_status=400)
+        result = refresh_token(initial_token, expected_status=401)
 
-    assert "expired" in result["nonFieldErrors"][0]
+    assert "detail" in result
 
 
 def test_received_token_works(as_anon, refresh_token, initial_token):
-    token = refresh_token(initial_token)["token"]
-    as_anon.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+    token = refresh_token(initial_token)["access"]
+    as_anon.credentials(HTTP_AUTHORIZATION=f"JWT {token}")
 
     result = as_anon.get("/api/v1/users/me/")
 
--- src/app/conf/auth.py
+++ src/app/conf/auth.py
@@ -10,14 +10,9 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
-# JWT_AUTH = {
-#     "JWT_EXPIRATION_DELTA": timedelta(days=14),
-#     "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=21),
-#     "JWT_ALLOW_REFRESH": True,
-# }
 
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ('JWT',),
+    "AUTH_HEADER_TYPES": ("JWT",),
 }
 
 
