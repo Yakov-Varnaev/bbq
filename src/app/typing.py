4c37fbc9847939403c64e7508987d5588fff585f
Yakov Varnaev <yakov.varnaev@gmail.com>
2023-10-13 00:51:37 +0300

    lint

--- src/app/typing.py
+++ src/app/typing.py
@@ -5,15 +5,6 @@ from rest_framework.serializers import BaseSerializer
 
 from django.db.models import Model, QuerySet
 
-RestPageAssertion = Callable[
-    [
-        dict,
-        QuerySet | list[Model],
-        type[BaseSerializer],
-        str | None,
-        str | None,
-    ],
-    None,
-]
+RestPageAssertion = Callable[[dict, QuerySet | list[Model], type[BaseSerializer], str | None, str | None], None]
 ExistCheckAssertion = Callable[[Arg(type[Model], "model"), KwArg()], None]  # noqa: F821
 ModelAssertion = Callable[[Arg(dict, "data"), KwArg()], None]  # noqa: F821
