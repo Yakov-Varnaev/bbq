350fd0e6f68b2a1374242a12b70102d447fcb34d
Вячеслав Менюхов <menyukhov@bk.ru>
2023-12-26 23:16:35 +0300

    Add factory and fixtures packages.

--- src/companies/fixtures/stock.py
+++ src/companies/fixtures/stock.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.testing.factory import FixtureFactory
 from companies.models.point import Point
+from companies.models.stock import Stock, StockMaterial
 
 
 @pytest.fixture
@@ -17,3 +18,8 @@ def stock(factory: FixtureFactory, company_point: Point) -> dict:
 @pytest.fixture
 def material(factory: FixtureFactory) -> dict:
     return factory.material()
+
+
+@pytest.fixture
+def stock_material(factory: FixtureFactory, stock: Stock) -> StockMaterial:
+    return factory.stock_material(stock=stock)
