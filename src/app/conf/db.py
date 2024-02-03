2e0ecaeebaa0b3b8398515f9699d4794f759a57d
Yakov Varnaev <yakov.varnaev@gmail.com>
2024-02-03 22:58:01 +0300

    fix mypy and pytest

--- src/app/conf/db.py
+++ src/app/conf/db.py
@@ -5,7 +5,7 @@ from app.conf.environ import env
 
 DATABASES = {
     # read os.environ["DATABASE_URL"] and raises ImproperlyConfigured exception if not found
-    "default": env.db(),
+    "default": env.db(default="sqlite:///db.sqlite"),
 }
 
 # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
