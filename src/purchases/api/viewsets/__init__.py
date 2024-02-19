7c1a09d78e16e539b4e7bf451d67ae9cfc716707
Вячеслав Менюхов <menyukhov@bk.ru>
2024-02-19 09:04:22 +0300

    Add PurchaseProcedureViewSet.

--- src/purchases/api/viewsets/__init__.py
+++ src/purchases/api/viewsets/__init__.py
@@ -1,7 +1,9 @@
 from purchases.api.viewsets.product_material import ProductMaterialViewSet
 from purchases.api.viewsets.purchase import PurchaseViewSet
+from purchases.api.viewsets.purchase_procedure import PurchaseProcedureViewSet
 
 __all__ = [
     "ProductMaterialViewSet",
     "PurchaseViewSet",
+    "PurchaseProcedureViewSet",
 ]
---
+++ src/purchases/api/viewsets/purchase_procedure.py
@@ -0,0 +1,26 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.viewsets import ModelViewSet
+
+from django.db.models.query import QuerySet
+
+from app.api.permissions import IsCompanyOwnerOrReadOnly
+from purchases.api.serializers import PurchaseProcedureReadSerializer, PurchaseProcedureWriteSerializer
+from purchases.models import PurchaseProcedure
+
+
+@extend_schema(tags=["procedures-purchase"])
+class PurchaseProcedureViewSet(ModelViewSet):
+    permission_classes = [IsCompanyOwnerOrReadOnly]
+
+    def get_queryset(self) -> QuerySet[PurchaseProcedure]:
+        return (
+            PurchaseProcedure.objects.select_related("purchase")
+            .point(self.kwargs["company_pk"], self.kwargs["point_pk"])
+            .with_material_info()
+        )
+
+    def get_serializer_class(self) -> type[PurchaseProcedureReadSerializer | PurchaseProcedureWriteSerializer]:
+        if self.request.method in SAFE_METHODS:
+            return PurchaseProcedureReadSerializer
+        return PurchaseProcedureWriteSerializer
